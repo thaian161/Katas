c05ddd1635a6aa899dea8993226ab4726eb700e4
thaian161 <thaian161@yahoo.com>
2022-05-21 17:40:33 -0500

    repeating numbers in array

---
+++ kata8-repeatingNum.js
@@ -0,0 +1,30 @@
+//The input data for this exercise will be two dimensional array (an array of arrays), where each sub-array will have two numeric values. The first will be the value to repeat, the second will be the amount of times to repeat that value.
+//================================================================================
+
+const repeatNumbers = function (data) {
+  let result = [];
+
+  for (let r = 0; r < data.length; r++) {
+    for (let c = 0; c < data[r][1]; c++) {
+      result.push(data[r][0]);
+    }
+  }
+  return result.toString();
+};
+
+console.log(repeatNumbers([[1, 10]]));
+
+console.log(
+  repeatNumbers([
+    [1, 2],
+    [2, 3],
+  ])
+);
+
+console.log(
+  repeatNumbers([
+    [10, 4],
+    [34, 6],
+    [92, 2],
+  ])
+);
