717abbd9ecd0c40aea3d3a0c418bfde141b84121
thaian161 <thaian161@yahoo.com>
2022-05-21 16:41:01 -0500

    find the 2 largest number in an array and sum it

---
+++ Kata1-sumLargestNum.js
@@ -0,0 +1,11 @@
+// In this exercise, we will be given an array of 2 or more numbers. We will then have to find the two largest numbers in that array, and sum them together
+//=============================================================================================================
+
+const sumLargestNumbers = function (data) {
+  newData = data.sort((a, b) => b - a);
+  return newData[0] + newData[1];
+};
+
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
