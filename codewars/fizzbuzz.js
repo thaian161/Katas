d063568c97a00a312fcdb96569016da93f925222
thaian161 <thaian161@yahoo.com>
2022-09-20 23:07:42 -0500

    complete fizzbuzz with 2 solution

--- codewars/fizzbuzz.js
+++ codewars/fizzbuzz.js
@@ -10,10 +10,23 @@ If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
 N will never be less than 1.
 */
 
-const fizzBuzz=(n)=>{
-  let result="";
-  
-  for (let i = 0; i <n; i ++){
-    
+const fizzBuzz = (n) => {
+  let result = [];
+
+  for (let i = 0; i < n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push('fizzbuzz');
+    } else if (i % 3===0) {
+      result.push('fizz');
+    } else if (i % 5===0) {
+      result.push('buzz');
+    } else {
+      result.push(i);
+    }
   }
-}
+  return console.log(result);
+};
+
+fizzBuzz(10);
+
+// for (let i = 1; i < 101;) console.log((i++ % 3 ? '' : 'fizz') + (i % 5 ? '' : 'buzz') || i)
